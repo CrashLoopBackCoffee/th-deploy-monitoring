30481d955533868ee18b7b6d33a79129d89cf097
Tobias Henkel <tobias.henkel@bmw.de>
2023-10-29 19:54:35 +0100

    Add a speedtest exporter
    This will periodically check the internet speed to gather
    statistics.

---
+++ monitoring/speedtest.py
@@ -0,0 +1,34 @@
+"""
+Deploys the speedtest-exporter
+"""
+import pulumi_docker
+from pulumi import ResourceOptions
+
+from monitoring.utils import get_image
+
+
+def create_speedtest_exporter(network: pulumi_docker.Network, opts: ResourceOptions):
+    """
+    Deploys the speedtest-exporter
+    """
+    speedtest_exporter_image = pulumi_docker.RemoteImage(
+        "speedtest-exporter",
+        name=get_image("speedtest-exporter"),
+        keep_locally=True,
+        opts=opts,
+    )
+    pulumi_docker.Container(
+        "speedtest-exporter",
+        image=speedtest_exporter_image.image_id,
+        ports=[
+            pulumi_docker.ContainerPortArgs(internal=9469, external=9469),
+        ],
+        networks_advanced=[
+            pulumi_docker.ContainerNetworksAdvancedArgs(
+                name=network.name, aliases=["speedtest-exporter"]
+            )
+        ],
+        restart="always",
+        start=True,
+        opts=opts,
+    )
